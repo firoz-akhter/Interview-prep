46fd8c300e60e3f6224dfdd7105117968f658233
Firoz Akhter <firozakhter789@gmail.comgit config --list>
2025-11-02 09:48:54 +0530

    javascript basics practice started

--- javascript/first.js
+++ javascript/first.js
@@ -1,1 +1,7 @@
-console.log("hello there");
+// Write a javascript program to check two numbers and return true
+// if one of two numbers are 100 or sum of two numbers is 100
+
+const isEqualTo100 = (a, b) => a === 100 || b === 100 || a + b === 100;
+
+console.log(isEqualTo100(100, 0));
+console.log(isEqualTo100(10, 1));
