fd9244544f536ead8d69dd649b0ee873ab14e8b7
Firoz Akhter <firozakhter789@gmail.comgit config --list>
2025-11-02 10:06:21 +0530

    get file extension program addition practice

--- javascript/first.js
+++ javascript/first.js
@@ -5,3 +5,16 @@ const isEqualTo100 = (a, b) => a === 100 || b === 100 || a + b === 100;
 
 console.log(isEqualTo100(100, 0));
 console.log(isEqualTo100(10, 1));
+
+const name = "firoz akhter";
+
+// slice means kat ke nikalna
+console.log(name.slice(0, 5));
+
+// write a javascript program to get the extension of a file name
+
+// index.html
+const getFileExtension = (str) => str.slice(str.lastIndexOf("."));
+
+console.log(getFileExtension("index.html"));
+console.log(getFileExtension("webpack.config.js"));
